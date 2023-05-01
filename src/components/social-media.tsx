18c3624fe59f2bd830dce7cccec2c246d34dadb8
m7he4rt <mateuspm2015@hotmail.com>
2023-05-01 03:32:14 -0300

    feat: create social media component

---
+++ src/components/social-media.tsx
@@ -0,0 +1,43 @@
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+} from "react-icons/ai";
+
+import type { ReactNode } from "react";
+
+type LinksProps = {
+  icon: ReactNode;
+  link: string;
+  name: string;
+};
+
+const links: LinksProps[] = [
+  {
+    icon: <AiOutlineGithub className="text-[#FF2B51]" size={36} />,
+    link: "https://github.com/m7he4rt",
+    name: "Github",
+  },
+  {
+    icon: <AiOutlineTwitter className="text-white" size={36} />,
+    link: "https://twitter.com/m7ez1n",
+    name: "Twitter",
+  },
+  {
+    icon: <AiFillLinkedin className="text-[#FF2B51]" size={36} />,
+    link: "https://www.linkedin.com/in/m7aei",
+    name: "Linkedin",
+  },
+];
+
+const SocialMedia = () => (
+  <div className="flex gap-4">
+    {links.map(({ icon, link, name }) => (
+      <a key={`key-${name}`} href={link} target="_blank">
+        {icon}
+      </a>
+    ))}
+  </div>
+);
+
+export default SocialMedia;
