02bdf9db1f385eb6c7a8210dcce02fff995c7979
m7he4rt <mateuspm2015@hotmail.com>
2023-05-02 22:35:42 -0300

    feat: add my image

--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 import styles from "@/styles/circle.module.css";
 
-const Circle = () => <div className={styles.circle} />;
+const Circle = () => (
+  <div className="absolute bottom-0">
+    <div className={styles.circle}></div>
+    <Image
+      src="/eu.png"
+      alt="Mateus Pinheiro"
+      width={665}
+      height={795}
+      className="relative"
+    />
+  </div>
+);
 
 export default Circle;
