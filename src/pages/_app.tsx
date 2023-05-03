98f8a28a6ce082a0f18150ec32b0909f1b4b470d
m7he4rt <mateuspm2015@hotmail.com>
2023-05-03 04:41:17 -0300

    feat: add SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 
 import "@/styles/globals.css";
@@ -6,6 +7,21 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <DefaultSeo
+        title="Mateus Pinheiro"
+        description="Sou um jovem programador apaixonado por esse mundo e tentando ser um pouco melhor a cada dia."
+        openGraph={{
+          type: "website",
+          locale: "pt_BR",
+          url: "https://m7he4rt.vercel.app",
+          siteName: "m7he4rt",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "/eu.png",
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
