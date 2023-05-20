35480262bbf7636d1ffe534e3f8f4c3496d6d69c
m7he4rt <mateuspm2015@hotmail.com>
2023-05-19 23:52:14 -0300

    chore: locales texts

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  trailingSlash: true
 }
 
 module.exports = nextConfig
