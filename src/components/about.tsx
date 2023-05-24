f6c382be6659eb951db7f2d9be12ba001c848d04
m7he4rt <mateuspm2015@hotmail.com>
2023-05-23 22:20:58 -0300

    fix: ssr rendering texts

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const AboutMe = () => {
   const { t } = useTranslation();
+  const [texts, setTexts] = useState<{ title: string; description: string }>();
+
+  useEffect(
+    () =>
+      setTexts({
+        title: t("about.title"),
+        description: t("about.description"),
+      }),
+    [t]
+  );
 
   return (
     <div className="flex flex-col gap-2 text-center md:text-left w-96">
       <p className="text-3xl font-semibold leading-10 text-[#FF2B51]">
-        {t("about.title")}
+        {texts?.title}
       </p>
       <p className="text-xl font-normal leading-10 text-black dark:text-white">
-        {t("about.description")}
+        {texts?.description}
       </p>
     </div>
   );
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,6 +3,7 @@ import { AiOutlineGithub } from "react-icons/ai";
 
 import TechTag from "@/components/tech-tag";
 import { FiExternalLink } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
 type CardProjectProps = {
   name: string;
@@ -58,43 +59,48 @@ const CardProject = ({
 
 const Projects = () => {
   const { t } = useTranslation();
+  const [mockProjects, setMockProjects] = useState<CardProjectProps[]>([]);
 
-  const mockProjects: CardProjectProps[] = [
-    {
-      name: "He4rt Landing",
-      github: "https://github.com/he4rt/he4rt-landing-ssr",
-      url: "https://heartdevs.com",
-      description: t("projects.landing"),
-      techs: ["remix", "reactjs", "tailwindcss", "typescript"],
-    },
-    {
-      name: "He4rt Conf",
-      github: "https://github.com/he4rt/he4rtconf-landing",
-      url: "https://conf.heartdevs.com",
-      description: t("projects.conf"),
-      techs: ["nextjs", "reactjs", "styled-components", "typescript"],
-    },
-    {
-      name: "He4rt Recap 2022",
-      github: "https://github.com/kjkGustavo/he4rt-recap-2022",
-      url: "https://2022.heartdevs.com",
-      description: t("projects.recap"),
-      techs: [
-        "nextjs",
-        "reactjs",
-        "tailwindcss",
-        "typescript",
-        "framer-motion",
-      ],
-    },
-    {
-      name: "React 4noobs",
-      github: "https://github.com/he4rt/react4noobs",
-      url: "https://github.com/he4rt/react4noobs",
-      description: t("projects.4noobs"),
-      techs: ["reactjs", "markdown"],
-    },
-  ];
+  useEffect(
+    () =>
+      setMockProjects([
+        {
+          name: "He4rt Landing",
+          github: "https://github.com/he4rt/he4rt-landing-ssr",
+          url: "https://heartdevs.com",
+          description: t("projects.landing"),
+          techs: ["remix", "reactjs", "tailwindcss", "typescript"],
+        },
+        {
+          name: "He4rt Conf",
+          github: "https://github.com/he4rt/he4rtconf-landing",
+          url: "https://conf.heartdevs.com",
+          description: t("projects.conf"),
+          techs: ["nextjs", "reactjs", "styled-components", "typescript"],
+        },
+        {
+          name: "He4rt Recap 2022",
+          github: "https://github.com/kjkGustavo/he4rt-recap-2022",
+          url: "https://2022.heartdevs.com",
+          description: t("projects.recap"),
+          techs: [
+            "nextjs",
+            "reactjs",
+            "tailwindcss",
+            "typescript",
+            "framer-motion",
+          ],
+        },
+        {
+          name: "React 4noobs",
+          github: "https://github.com/he4rt/react4noobs",
+          url: "https://github.com/he4rt/react4noobs",
+          description: t("projects.4noobs"),
+          techs: ["reactjs", "markdown"],
+        },
+      ]),
+    [t]
+  );
 
   return (
     <div className="grid grid-flow-col grid-rows-4 gap-4 md:grid-rows-2">
