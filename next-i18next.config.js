ed8e704fffaf7494911a2a36dc70c94b6eb8653e
m7he4rt <mateuspm2015@hotmail.com>
2023-05-23 22:00:12 -0300

    fix: warning i18n next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-  i18n: {
-    defaultLocale: "pt-BR",
-    locales: ["pt-BR", "en-US"],
-    localePath: "./locales",
-  },
+/** @type import("next").I18NConfig */
+const i18n= {
+  defaultLocale: "pt-BR",
+  locales: ["pt-BR", "en-US"],
+}
+
+/** @type import("next-i18next").UserConfig */
+const next18nextConfig = {
+	i18n,
+	keySeparator: ".",
+	nsSeparator: ":",
+	localePath: "./locales",
+	reloadOnPrerender: process.env.NODE_ENV === "development"
 };
+
+module.exports = next18nextConfig;
