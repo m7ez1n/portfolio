87fc982d6f50588ae9c83de5625ff2bbc0947cdf
m7he4rt <mateuspm2015@hotmail.com>
2023-10-29 19:28:30 -0300

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const { i18n } = require("./next-i18next.config");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-  trailingSlash: true
-}
+const nextConfig = {}
 
 module.exports = nextConfig
